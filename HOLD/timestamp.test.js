0d9eb032ce519c9412ab6313fa4ec63f2847d576
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-18 15:41:50 -0700

    Timestamp test added and passing

--- HOLD/timestamp.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const timestamp = require('../lib/middleware/timestamp.js');
-
-describe('Testing Timestamp Middleware Functionality', () => {
-
-});
---
+++ __tests__/timestamp.test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const timestamp = require('../lib/middleware/timestamp.js');
+
+describe('Testing Timestamp Middleware Functionality', () => {
+
+
+    it('Should properly add a timestamp to the request', () => {
+        let req = {
+            body: 'some dummy data',
+            count: 12312,
+            path: '/blah/test/blah',
+        };
+
+        let res = {};
+        let next = jest.fn();
+        timestamp(req, res, next);
+
+        let actual = res.requestTime;
+
+        expect(actual).not.toBeNull();
+
+    });
+
+});
