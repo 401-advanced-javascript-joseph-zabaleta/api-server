aa1242f8f3d2cdaff6dc70666e2b3bd0cb61940a
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-08-19 23:20:51 -0700

    moved server.test.js

---
+++ __tests__/server.test.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { server } = require('../lib/models/server.js');
+const supertest = require('supertest');
+require('@code-fellows/supergoose');
+const mockRequest = supertest(server);
+
+let spyLog = jest.spyOn(console, 'log');
+let spyError = jest.spyOn(console, 'error');
+
+beforeEach( () => {
+    spyLog.mockReset();
+    spyError.mockReset();
+});
+
+describe('Testing server IT functionality:', () => {
+
+    it('Should respond with a 404 on an invalid route', () => {
+
+    return mockRequest
+        .get('/foobar')
+        .then(results => {
+            expect(results.status).toBe(404);
+        })
+
+    });
+
+    it('Should respond with a 404 on an invalid method', () => {
+
+    return mockRequest
+        .post('/')
+        .then(results => {
+            expect(results.status).toBe(404);
+        })
+
+    });
+
+});
+
+
